080ad3a28c761b28c5f7948a5c275791bdb905eb
kutoru <eg_bls@mail.ru>
2024-08-11 00:54:14 +0300

    Fix getNotes route & Add query building

--- app/src/main/java/com/kutoru/mikunotes/logic/requests/notes.kt
+++ app/src/main/java/com/kutoru/mikunotes/logic/requests/notes.kt
@@ -6,10 +6,43 @@ import com.kutoru.mikunotes.models.NotePost
 import com.kutoru.mikunotes.models.NoteQueryParameters
 import com.kutoru.mikunotes.models.NoteTagPost
 import io.ktor.http.HttpMethod
+import java.net.URLEncoder
 
-suspend fun RequestManager.getNotes(queryParams: NoteQueryParameters): MutableList<Note> {
-    val queryString = "?"
-    return executeRequestUntilBody<NoteGet>("$apiUrl/notes$queryString", HttpMethod.Get).notes
+suspend fun RequestManager.getNotes(queryParams: NoteQueryParameters): Pair<MutableList<Note>, UInt> {
+    var queryString = "?"
+
+    if (queryParams.page != null) {
+        queryString += "&page=${queryParams.page}"
+    }
+    if (queryParams.per_page != null) {
+        queryString += "&per_page=${queryParams.per_page}"
+    }
+    if (queryParams.sort_by != null) {
+        queryString += "&sort_by=${queryParams.sort_by}"
+    }
+    if (queryParams.sort_type != null) {
+        queryString += "&sort_type=${queryParams.sort_type}"
+    }
+    if (queryParams.tags != null) {
+        val tagsValue = queryParams.tags
+            .map { it.toString() }
+            .reduceOrNull { acc, v -> "$acc,$v" }
+            .orEmpty()
+
+        queryString += "&tags=$tagsValue"
+    }
+    if (queryParams.date != null) {
+        queryString += "&date=${queryParams.date.first}-${queryParams.date.second}"
+    }
+    if (queryParams.date_modif != null) {
+        queryString += "&date_modif=${queryParams.date_modif.first}-${queryParams.date_modif.second}"
+    }
+    if (queryParams.title != null) {
+        queryString += "&title=${URLEncoder.encode(queryParams.title, "utf-8")}"
+    }
+
+    val result = executeRequestUntilBody<NoteGet>("$apiUrl/notes$queryString", HttpMethod.Get)
+    return Pair(result.notes, result.total_count)
 }
 
 suspend fun RequestManager.postNotes(body: NotePost): Note {
--- app/src/main/java/com/kutoru/mikunotes/models/NoteGet.kt
+++ app/src/main/java/com/kutoru/mikunotes/models/NoteGet.kt
@@ -5,4 +5,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class NoteGet(
     val notes: MutableList<Note>,
+    val total_count: UInt,
 )
